cd8b28821247193dae2c900c55213cde04603e7c
lucpro9001 <lucpro9001@gmail.com>
2022-04-08 13:51:40 +0700

    update part 2

---
+++ demo-2/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List < Employee > getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
